314db4690178af32f4e170acbe32b02eb51abb7b
Ashwanikrs <ashwaniksingh111@gmail.com>
2024-12-24 21:46:47 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,45 @@
+import os
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import generate  # Backend caption generation logic
+
+app = Flask(__name__)
+
+# Directory to save uploaded images
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Create the upload folder if it doesn't exist
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+
+# Home route
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+# Handle file upload and caption generation
+@app.route('/generate_caption', methods=['POST'])
+@app.route('/generate_caption', methods=['POST'])
+def generate_caption():
+    if 'file' not in request.files:
+        return "No file part"
+    file = request.files['file']
+    if file.filename == '':
+        return "No selected file"
+    if file:
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+
+        # Generate caption using the backend
+        caption = generate.runModel(file_path)
+
+        # Return template with the filename and caption
+        return render_template('index.html', filename=filename, caption=caption)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
